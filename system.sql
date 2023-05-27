49bb36bcea363f92ecfa355b81c517bf6c029168
hoangphuc552001 <hoangphuc552001@gmail.com>
2023-05-28 00:39:06 +0700

    modify code from review

--- system.sql
+++ system.sql
@@ -1,20 +1,20 @@
 create table if not exists Professor
 (
-    prof_id    int primary key,
+    prof_id    int primary key auto_increment,
     prof_lname varchar(50),
     prof_fname varchar(50)
     );
 
 create table if not exists Course
 (
-    course_id   int primary key,
+    course_id   int primary key auto_increment,
     course_name varchar(255)
     );
 
 
 create table if not exists Room
 (
-    room_id  int primary key,
+    room_id  int primary key auto_increment,
     room_loc varchar(50),
     room_cap varchar(50),
     class_id int
@@ -22,7 +22,7 @@ create table if not exists Room
 
 create table if not exists Class
 (
-    class_id   int primary key,
+    class_id   int primary key auto_increment,
     class_name varchar(255),
     prof_id    int,
     course_id  int,
@@ -38,7 +38,7 @@ alter table Room
 
 create table if not exists Student
 (
-    stud_id     int primary key,
+    stud_id     int primary key auto_increment,
     stud_fname  varchar(50),
     stud_lname  varchar(50),
     stud_street varchar(255),
@@ -56,24 +56,24 @@ create table if not exists Enroll
     FOREIGN KEY (class_id) REFERENCES Class (class_id)
     );
 
-INSERT INTO Professor (prof_id, prof_lname, prof_fname)
-VALUES (1, 'Smith', 'John'),
-       (2, 'Johnson', 'Mary'),
-       (3, 'Davis', 'David');
-INSERT INTO Course (course_id, course_name)
-VALUES (1, 'Mathematics'),
-       (2, 'English'),
-       (3, 'Science');
+INSERT INTO Professor (prof_lname, prof_fname)
+VALUES ('Smith', 'John'),
+       ('Johnson', 'Mary'),
+       ('Davis', 'David');
+INSERT INTO Course (course_name)
+VALUES ('Mathematics'),
+       ('English'),
+       ('Science');
 
 
-INSERT INTO Room (room_id, room_loc, room_cap)
-VALUES (1, 'Building A, Room 101', '30'),
-       (2, 'Building B, Room 201', '25'),
-       (3, 'Building C, Room 301', '35');
-INSERT INTO Class (class_id, class_name, prof_id, course_id, room_id)
-VALUES (1, 'Class 1', 1, 1, 1),
-       (2, 'Class 2', 2, 2, 2),
-       (3, 'Class 3', 3, 3, 3);
+INSERT INTO Room (room_loc, room_cap)
+VALUES ('Building A, Room 101', '30'),
+       ('Building B, Room 201', '25'),
+       ('Building C, Room 301', '35');
+INSERT INTO Class (class_name, prof_id, course_id, room_id)
+VALUES ('Class 1', 1, 1, 1),
+       ('Class 2', 2, 2, 2),
+       ('Class 3', 3, 3, 3);
 
 update Room
 set class_id=1
@@ -88,19 +88,32 @@ set class_id=3
 where room_id = 3;
 
 
-INSERT INTO Student (stud_id, stud_fname, stud_lname, stud_street, stud_city, stud_zip)
-VALUES (1, 'Alice', 'Johnson', '123 Main St', 'New York', '10001'),
-       (2, 'Bob', 'Smith', '456 Elm St', 'Los Angeles', '90001'),
-       (3, 'Charlie', 'Davis', '789 Oak St', 'Chicago', '60601');
-INSERT INTO Enroll (stud_id, class_id, grade)
-VALUES (1, 1, 'A'),
-       (2, 2, 'B'),
-       (3, 3, 'C');
+INSERT INTO Student (stud_fname, stud_lname, stud_street, stud_city, stud_zip)
+VALUES ('Alice', 'Johnson', '123 Main St', 'New York', '10001'),
+       ('Bob', 'Smith', '456 Elm St', 'Los Angeles', '90001'),
+       ('Charlie', 'Davis', '789 Oak St', 'Chicago', '60601');
+INSERT INTO Enroll (class_id, grade)
+VALUES (1, 'A'),
+       (2, 'B'),
+       (3, 'C');
+
+Create OR REPLACE VIEW Grade AS
+SELECT e.class_id,e.stud_id,
+       CASE e.grade
+           WHEN 'A' THEN 10
+           WHEN 'B' THEN 8
+           WHEN 'C' THEN 6
+           WHEN 'D' THEN 4
+           WHEN 'E' THEN 2
+           WHEN 'F' THEN 0
+           ELSE 0
+           END
+    AS 'Grade'
+FROM Enroll e
+
 
 #1
-select CONCAT(p.prof_fname, ' ', p.prof_lname) as 'Professor Name',
-        CONCAT(s.stud_fname, ' ', s.stud_lname) as 'Student Name',
-        c.class_name                            as 'Class Name'
+select CONCAT(p.prof_fname, ' ', p.prof_lname) as 'Professor Name', CONCAT(s.stud_fname, ' ', s.stud_lname) as 'Student Name', c.class_name as 'Class Name'
 from Professor p
          join Class c on p.prof_id = c.prof_id
          join Enroll E on c.class_id = E.class_id
@@ -114,74 +127,42 @@ select distinct(co.course_name)
 from Course co
          join Class c on co.course_id = c.course_id
          join Enroll E on c.class_id = E.class_id
+
 #4
-SELECT distinct(CONCAT(s.stud_fname, ' ', s.stud_lname)) AS 'Student Name',
-        AvgGrade.Grade                            AS 'Average Grade',
+select CONCAT(s.stud_fname, ' ', s.stud_lname) AS 'Student Name',
+        AVG(G.Grade)                            AS 'Average Grade',
         CASE
-            WHEN AvgGrade.Grade < 5 THEN 'Weak'
-            WHEN AvgGrade.Grade >= 5 AND AvgGrade.Grade < 8 THEN 'Average'
-            WHEN AvgGrade.Grade >= 8 THEN 'Good'
-            END                                   AS 'Grade'
-FROM Student s
-         JOIN Enroll e ON s.stud_id = e.stud_id
-         JOIN (SELECT e.stud_id,
-                      AVG(
-                              CASE e.grade
-                                  WHEN 'A' THEN 10
-                                  WHEN 'B' THEN 8
-                                  WHEN 'C' THEN 6
-                                  WHEN 'D' THEN 4
-                                  WHEN 'E' THEN 2
-                                  WHEN 'F' THEN 0
-                                  ELSE 0
-                                  END
-                          ) AS 'Grade'
-               FROM Enroll e
-               GROUP BY stud_id) AS AvgGrade ON s.stud_id = AvgGrade.stud_id;
+            WHEN AVG(G.Grade) < 5 THEN 'Weak'
+            WHEN AVG(G.Grade) >= 5 AND AVG(G.Grade) < 8 THEN 'Average'
+            WHEN AVG(G.Grade) >= 8 THEN 'Good'
+            END                                 AS 'Grade'
+from Student s
+         join Grade G on s.stud_id = G.stud_id
+group by s.stud_id
+
 
 #5
-select c.class_name,
-       AvgGrade.Grade as 'Grade',
+select c.class_name AS 'Class Name',
+        AVG(G.Grade) AS 'Average Grade',
         CASE
-            WHEN AvgGrade.Grade < 5 THEN 'Weak'
-            WHEN AvgGrade.Grade >= 5 AND AvgGrade.Grade < 8 THEN 'Average'
-            WHEN AvgGrade.Grade >= 8 THEN 'Good'
-            END AS 'Grade'
+            WHEN AVG(G.Grade) < 5 THEN 'Weak'
+            WHEN AVG(G.Grade) >= 5 AND AVG(G.Grade) < 8 THEN 'Average'
+            WHEN AVG(G.Grade) >= 8 THEN 'Good'
+            END      AS 'Grade'
 from Class c
-         JOIN (SELECT e.class_id,
-                      AVG(
-                              CASE e.grade
-                                  WHEN 'A' THEN 10
-                                  WHEN 'B' THEN 8
-                                  WHEN 'C' THEN 6
-                                  WHEN 'D' THEN 4
-                                  WHEN 'E' THEN 2
-                                  WHEN 'F' THEN 0
-                                  ELSE 0
-                                  END
-                          ) AS 'Grade'
-               FROM Enroll e
-               GROUP BY e.class_id) AS AvgGrade ON c.class_id = AvgGrade.class_id
+         join Grade G on c.class_id = G.class_id
+group by c.class_id, c.class_name
+
+
 #6
-select co.course_name,
-       AvgGrade.Grade as 'Grade',
+select c.course_name as 'Course Name',
+        AVG(G.Grade) AS 'Average Grade',
         CASE
-            WHEN AvgGrade.Grade < 5 THEN 'Weak'
-            WHEN AvgGrade.Grade >= 5 AND AvgGrade.Grade < 8 THEN 'Average'
-            WHEN AvgGrade.Grade >= 8 THEN 'Good'
-            END AS 'Grade'
-from Course co
-         JOIN (SELECT coo.course_id,
-                      AVG(
-                              CASE e.grade
-                                  WHEN 'A' THEN 10
-                                  WHEN 'B' THEN 8
-                                  WHEN 'C' THEN 6
-                                  WHEN 'D' THEN 4
-                                  WHEN 'E' THEN 2
-                                  WHEN 'F' THEN 0
-                                  ELSE 0
-                                  END
-                          ) AS 'Grade'
-               FROM Enroll e join Course coo on e.class_id = coo.course_id
-               GROUP BY coo.course_id) AS AvgGrade ON co.course_id = AvgGrade.course_id
+            WHEN AVG(G.Grade) < 5 THEN 'Weak'
+            WHEN AVG(G.Grade) >= 5 AND AVG(G.Grade) < 8 THEN 'Average'
+            WHEN AVG(G.Grade) >= 8 THEN 'Good'
+            END      AS 'Grade'
+from Course c join Class C2 on c.course_id = C2.course_id
+              join Grade G on C2.class_id = G.class_id
+group by c.course_id, c.course_name
+
