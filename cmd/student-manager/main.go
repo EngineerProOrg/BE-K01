482204e8a915cf7312d8682b4e593e6e578b27cc
quangmx <maxuanquang@gmail.com>
2023-05-31 16:00:24 +0700

    Fix bug to run

--- cmd/student-manager/main.go
+++ cmd/student-manager/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
+	// "flag"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 
 	"github.com/EngineerProOrg/BE-K01/configs"
-	"github.com/EngineerProOrg/BE-K01/pkg/controller"
+	// "github.com/EngineerProOrg/BE-K01/pkg/controller"
 	"github.com/EngineerProOrg/BE-K01/pkg/service"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
@@ -17,13 +17,13 @@ import (
 	"gorm.io/gorm"
 )
 
-var (
-	confPath = flag.String("conf", "files/live.json", "path to config file")
-)
+// var (
+// 	confPath = flag.String("conf", "files/live.json", "path to config file")
+// )
 
 func main() {
 	config := &configs.StudentManagerConfig{}
-	jsonFile, err := os.Open("users.json")
+	jsonFile, err := os.Open("configs/files/live.json")
 	// if we os.Open returns an error then handle it
 	if err != nil {
 		fmt.Println(err)
@@ -46,7 +46,7 @@ func main() {
 		ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
 	})
 	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN:                       "root:123456@tcp(127.0.0.1:3306)/engineerpro?charset=utf8mb4&parseTime=True&loc=Local",
+		DSN:                       "quangmx:2511@tcp(127.0.0.1:3306)/engineerpro?charset=utf8mb4&parseTime=True&loc=Local",
 		DefaultStringSize:         256,
 		DisableDatetimePrecision:  true,
 		DontSupportRenameIndex:    true,
@@ -62,7 +62,10 @@ func main() {
 	if rd == nil {
 		return
 	}
-	service := service.NewService(db, rd)
-	controller.MappingService(r, service)
+	// service := service.NewService(db, rd)
+	// controller.MappingService(r, service)
+
+	s := service.NewService(db, rd)
+	service.MappingService(r, s)
 	r.Run()
 }
