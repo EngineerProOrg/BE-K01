7e0f72f0c5222f8b6ee1311134e8befaddc2cd4d
phamvantuan <tuan.pv@neo-lab.vn>
2023-06-02 01:06:01 +0700

    homework

--- cmd/student-manager/main.go
+++ cmd/student-manager/main.go
@@ -1,52 +1,55 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 
-	"github.com/EngineerProOrg/BE-K01/configs"
-	"github.com/EngineerProOrg/BE-K01/pkg/controller"
-	"github.com/EngineerProOrg/BE-K01/pkg/service"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-var (
-	confPath = flag.String("conf", "files/live.json", "path to config file")
-)
+type Professor struct {
+	ProfId    int32  `gorm:"prof_id"`
+	ProfLname string `gorm:"prof_lname"`
+	ProfFname string `gorm:"prof_fname"`
+	NumClass  int32  `gorm:"num_class"`
+}
+
+func responseError(c *gin.Context, err string) {
+	c.JSON(http.StatusBadRequest, gin.H{
+		"error": err,
+	})
+}
+
+func getProfessors(db *gorm.DB) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var data []Professor
+
+		err := db.Table("professors as p").
+			Select("p.prof_id, p.prof_lname, p.prof_fname, COUNT(p.prof_id) as num_class").
+			Joins("JOIN classes as c ON c.prof_id = p.prof_id").
+			Joins("JOIN enrolls as e ON c.class_id = e.class_id").
+			Joins("JOIN students as s ON s.stud_id = e.stud_id").
+			Group("p.prof_id").
+			Find(&data).Error
+		if err != nil {
+			responseError(c, err.Error())
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": data,
+		})
+	}
+}
 
 func main() {
-	config := &configs.StudentManagerConfig{}
-	jsonFile, err := os.Open("users.json")
-	// if we os.Open returns an error then handle it
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer jsonFile.Close()
-	bt, err := io.ReadAll(jsonFile)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	err = json.Unmarshal(bt, config)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 
-	r := gin.Default()
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
-	})
+	// connect to mysql
 	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN:                       "root:123456@tcp(127.0.0.1:3306)/engineerpro?charset=utf8mb4&parseTime=True&loc=Local",
+		DSN:                       "root:12345@tcp(127.0.0.1:3305)/engineerpro?charset=utf8mb4&parseTime=True&loc=Local",
 		DefaultStringSize:         256,
 		DisableDatetimePrecision:  true,
 		DontSupportRenameIndex:    true,
@@ -54,15 +57,56 @@ func main() {
 	}), &gorm.Config{
 		SkipDefaultTransaction: true,
 	})
+
+	// connect to redis
+	rd := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6377",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	// config := &configs.StudentManagerConfig{}
+	// jsonFile, err := os.Open("users.json")
+	// // if we os.Open returns an error then handle it
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer jsonFile.Close()
+	// bt, err := io.ReadAll(jsonFile)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// err = json.Unmarshal(bt, config)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	r := gin.Default()
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		if db != nil {
+			fmt.Println("connected DB ")
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	r.GET("/professor", getProfessors(db))
+
 	if err != nil {
 		fmt.Println("can not connect to db ", err)
 		return
 	}
-	rd := redis.NewClient(&redis.Options{})
+
 	if rd == nil {
+		fmt.Println("connect fail to redis")
 		return
 	}
-	service := service.NewService(db, rd)
-	controller.MappingService(r, service)
+
+	// ab := service.GetProfessor(1)
+	// print(ab)
+	// MappingService(r, service)
 	r.Run()
 }
--- pkg/service/get_student.go
+++ pkg/service/get_student.go
@@ -1,6 +1,5 @@
 package service
 
 func (s *studentManagerService) GetStudent(id int64) interface{} {
-	//TODO implement me
-	panic("implement me")
+	s.repo.GetStudentByIdx(1)
 }
